1ac54be100d2ea75be6d5c9103b64c6c6cdda30e
LuizFernandez <62110246+LuizFernandez@users.noreply.github.com>
2020-03-12 20:52:57 +0000

    Função que imprime novo tabuleiro

--- Camada_Interface.c
+++ Camada_Interface.c
@@ -6,4 +6,15 @@
 
 
 // Função que permite mostrar o tabuleiro;
-void mostrar_tabuleiro(ESTADO estado);
+void mostrar_tabuleiro(ESTADO estado){
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            if (i == 0 && j == 7) printf("2 ");
+            else if (i == 7 && j == 0) printf("1 ");
+            else if (((&estado) -> tab [i][j]) == VAZIO) printf(". ");
+            else if (((&estado) -> tab [i][j]) == PRETA) printf("# ");
+            else if (((&estado) -> tab [i][j]) == BRANCA) printf("* ");
+        }
+        printf("\n");
+    }
+}
