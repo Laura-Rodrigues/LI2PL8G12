b32d6336e0af977c0a081fdc77c0c3bcdf172a98
LuizFernandez <62110246+LuizFernandez@users.noreply.github.com>
2020-03-12 20:52:13 +0000

    Update Camada_Interface.h

--- Camada_Interface.h
+++ Camada_Interface.h
@@ -28,17 +28,6 @@ typedef struct {
 } ESTADO;
 
 
-void mostrar_tabuleiro(ESTADO estado){
-    for (int i = 0; i < 8; i++) {
-        for (int j = 0; j < 8; j++) {
-            if (i == 0 && j == 7) printf("2 ");
-            else if (i == 7 && j == 0) printf("1 ");
-            else if (((&estado) -> tab [i][j]) == VAZIO) printf(". ");
-            else if (((&estado) -> tab [i][j]) == PRETA) printf("# ");
-            else if (((&estado) -> tab [i][j]) == BRANCA) printf("* ");
-        }
-        printf("\n");
-    }
-}
+void mostrar_tabuleiro(ESTADO estado);
 
 #endif //PROJETORASTROS_CAMADA_INTERFACE_H
