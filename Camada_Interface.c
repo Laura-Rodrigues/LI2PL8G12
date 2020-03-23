829acb0fa0fce61100612156b2fb5206c59bc4f1
LuizFernandez <62110246+LuizFernandez@users.noreply.github.com>
2020-03-23 20:45:40 +0000

    Ficheiro guarda tabuleiro inicial

--- Camada_Interface.c
+++ Camada_Interface.c
@@ -37,13 +37,34 @@ void prompt (ESTADO*e){
     printf ("#%02d PLAYER%d (%d) -> ", n_mov , njogador,numerojogadas);
 }
 
-int gr(FILE *ficheiro){
-    FILE *fp = fopen("ficheiro", "w");
+void fptabuleiro(FILE *Projeto_Rastos){
+    for (int i = 0; i < 8; i++) {
+        fprintf(Projeto_Rastos, "%c ", '8' - i );
+        for (int j = 0; j < 8; j++) {
+            COORDENADA c = {i,j};
+            if (obter_estado_casa(inicializar_estado(),c) == DOIS) fprintf(Projeto_Rastos, "2 ");
+            else if (obter_estado_casa(inicializar_estado() ,c) == UM) fprintf(Projeto_Rastos, "1 ");
+            else if (obter_estado_casa(inicializar_estado() ,c) == VAZIO) fprintf(Projeto_Rastos, ". ");
+            else if (obter_estado_casa(inicializar_estado() ,c) == PRETA) fprintf(Projeto_Rastos, "# ");
+            else if (obter_estado_casa(inicializar_estado() ,c) == BRANCA) fprintf(Projeto_Rastos, "* ");
+        }
+        fprintf(Projeto_Rastos, "\n");
+    }
+    fprintf(Projeto_Rastos, "  ");
+    for(int i = 0; i < 8; i++) {
+        fprintf(Projeto_Rastos, "%c ", 'a' + i);
+    };
+    fprintf(Projeto_Rastos, "\n");
+}
+
+
+int gr(FILE *Projeto_Rastos){
+    FILE *fp = fopen("Projeto_Rastos", "w");
     if(fp==NULL){
         fclose(fp);
         return 0;
     } else {
-        fprintf(fp,"Ola\n");
+        fptabuleiro(fp);
         fclose(fp);
     }
     return 1;
