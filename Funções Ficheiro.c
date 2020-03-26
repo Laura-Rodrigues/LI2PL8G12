f12dfb2adb3836685ad038f32ec95bd2230d40cc
LuizFernandez <62110246+LuizFernandez@users.noreply.github.com>
2020-03-26 19:58:38 +0000

    Add files via upload

---
+++ Funções Ficheiro.c
@@ -0,0 +1,53 @@
+//
+// Created by luis on 22/03/20.
+//
+
+#include <bits/types/FILE.h>
+#include "Gr e ler.h"
+#include "Camada de Interface.h"
+#include "Camada de dados.h"
+
+
+void fptabuleiro(FILE *Projeto_Rastos, ESTADO *e){
+    for (int i = 0; i < 8; i++) {
+        fprintf(Projeto_Rastos, "%c ", '8' - i );
+        for (int j = 0; j < 8; j++) {
+            COORDENADA c = {i,j};
+            if (obter_estado_casa(e,c) == DOIS) fprintf(Projeto_Rastos, "2 ");
+            else if (obter_estado_casa(e ,c) == UM) fprintf(Projeto_Rastos, "1 ");
+            else if (obter_estado_casa(e ,c) == VAZIO) fprintf(Projeto_Rastos, ". ");
+            else if (obter_estado_casa(e ,c) == PRETA) fprintf(Projeto_Rastos, "# ");
+            else if (obter_estado_casa(e ,c) == BRANCA) fprintf(Projeto_Rastos, "* ");
+        }
+        fprintf(Projeto_Rastos, "\n");
+    }
+    fprintf(Projeto_Rastos, "  ");
+    for(int i = 0; i < 8; i++) {
+        fprintf(Projeto_Rastos, "%c ", 'a' + i);
+    };
+    fprintf(Projeto_Rastos, "\n");
+}
+
+
+int gr(FILE *Projeto_Rastos, ESTADO *e){
+    FILE *fp = fopen("Projeto_Rastos", "w");
+    if(fp==NULL){
+        fclose(fp);
+        return 0;
+    } else {
+        fptabuleiro(fp,e);
+        fclose(fp);
+    }
+    return 1;
+}
+
+int ler(FILE *Projeto_Rastos){
+    FILE *fp = fopen("Projeto_Rastos", "r");
+    int c;
+    while((c = fgetc(fp)) != EOF){
+        printf("%c", c);
+    }
+    return 1;
+}
+
+
---
+++ Funções Ficheiro.h
@@ -0,0 +1,14 @@
+//
+// Created by luis on 22/03/20.
+//
+
+#ifndef PROJETOLI2_GR_E_LER_H
+#define PROJETOLI2_GR_E_LER_H
+
+#include "Camada de dados.h"
+
+void fptabuleiro(FILE *Projeto_Rastos, ESTADO *e);
+int gr(FILE *Projeto_Rastos, ESTADO *e);
+int ler(FILE *Projeto_Rastos);
+
+#endif //PROJETOLI2_GR_E_LER_H
