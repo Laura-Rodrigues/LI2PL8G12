95ca61326a3e127cd3e5270191825c66a182542c
MarianaFilipa <62109899+MarianaFilipa@users.noreply.github.com>
2020-04-03 11:49:29 +0100

    Update Funcoes_Ficheiro.c

--- Funcoes_Ficheiro.c
+++ Funcoes_Ficheiro.c
@@ -27,7 +27,7 @@ void fptabuleiro(FILE *Projeto_Rastros, ESTADO *e){
     fprintf(Projeto_Rastros, "\n");
 }
 
-int movsficheiro (ESTADO *e, FILE *nome){
+int movsficheiro (ESTADO *e, FILE *nome) {
     char c1, c2;
     int i, l1, l2;
     int jogada = obter_numero_de_jogadas(e);
@@ -46,6 +46,7 @@ int movsficheiro (ESTADO *e, FILE *nome){
             fprintf(nome,"%02d: %c%d %c%d \n", i+1, c1, l1, c2, l2);
 
     }
+    return 0;
 }
 
 int gr( char nomeficheiro[], ESTADO *e){
@@ -60,6 +61,7 @@ int gr( char nomeficheiro[], ESTADO *e){
     }
     return 1;
 }
+
 int ler(char nomeficheiro[], ESTADO *e){
     FILE *fp = fopen(nomeficheiro, "r");
     int c;
@@ -68,7 +70,7 @@ int ler(char nomeficheiro[], ESTADO *e){
     }
     return 1;
 }
-int movs ( ESTADO *e) {
+int movs (ESTADO *e) {
     char c1, c2;
     int i, l1, l2;
     int jogada = obter_numero_de_jogadas(e);
@@ -87,4 +89,27 @@ int movs ( ESTADO *e) {
             printf("%02d: %c%d %c%d \n", i+1, c1, l1, c2, l2);
 
     }
+    return 0;
+}
+
+int pos (ESTADO *e, int dado){
+    JOGADAS tmp;
+    int i, jogador;
+    for (i = 0; i < dado; i++){
+        tmp[i].jogador1 = obter_jogada(e,i,1);
+        tmp[i].jogador2 = obter_jogada(e,i,2);
+    }
+    altera_jogadoratual(e);
+    jogador = obter_jogador_atual(e);
+    e = inicializar_estado();
+    for ( i = 0; i < dado; i++) {
+        e->jogadas[i] = tmp[i];
+    }
+    e->num_jogadas = dado;
+    e->num_movimentos = 2*dado;
+    e->jogador_atual = jogador;
+    e->ultima_jogada = tmp[i].jogador2;
+    mostrar_tabuleiro(e);
+    printf("FEITO");
+    return 0;
 }
