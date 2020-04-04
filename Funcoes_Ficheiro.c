3bf3233927eff831c22b05b3b1d4a1f9b4f67853
MarianaFilipa <62109899+MarianaFilipa@users.noreply.github.com>
2020-04-04 19:32:20 +0100

    Alterações na pos e na movs

--- Funcoes_Ficheiro.c
+++ Funcoes_Ficheiro.c
@@ -72,46 +72,57 @@ int ler(char nomeficheiro[], ESTADO *e){
 
     return 1;
 }
+
+
 int movs (ESTADO *e) {
     char c1, c2;
     int i, l1, l2;
     int jogada = obter_numero_de_jogadas(e);
     int mov = obter_num_mov(e);
-    for ( i = 0; i < jogada; i++, mov -= 2) {
-        COORDENADA jog1 = obter_jogada(e, i, 1);
-        c1 = jog1.coluna + 'a';
-        l1 = jog1.linha + 1;
-        COORDENADA jog2 = obter_jogada(e, i, 2);
-        c2 = jog2.coluna + 'a';
-        l2 = jog2.linha + 1;
-        if (mov < 2 && mov %2 != 0){
-            printf("%02d: %c%d \n", i+1, c1, l1);
+    for ( i = 0; i <= jogada && mov > 0; i++) {
+        if ( i == 0) {
+            printf("00: e5 \n");
+            mov--;
         }
-        else
-            printf("%02d: %c%d %c%d \n", i+1, c1, l1, c2, l2);
-
+        else {
+            COORDENADA jog1 = obter_jogada(e, i, 1);
+            c1 = jog1.coluna + 'a';
+            l1 = jog1.linha + 1;
+            COORDENADA jog2 = obter_jogada(e, i, 2);
+            c2 = jog2.coluna + 'a';
+            l2 = jog2.linha + 1;
+            if ( mov < 2 && mov % 2 != 0)
+                printf("%02d: %c%d \n", i , c1, l1);
+            else
+                {
+                printf("%02d: %c%d %c%d \n", i, c1, l1, c2, l2);
+                mov -= 2;
+            }
+        }
     }
     return 0;
 }
 
-int pos (ESTADO *e, int dado){
-    JOGADAS tmp;
-    int i, jogador;
-    for (i = 0; i < dado; i++){
-        tmp[i].jogador1 = obter_jogada(e,i,1);
-        tmp[i].jogador2 = obter_jogada(e,i,2);
+
+
+ESTADO* pos (ESTADO *e, int dado){
+    int m = obter_num_mov(e);
+    JOGADAS t;
+    for (int i = 1;i <= dado; i++) {
+        t[i].jogador1 = obter_jogada(e, i, 1);
+        t[i].jogador2 = obter_jogada(e, i, 2);
     }
-    altera_jogadoratual(e);
-    jogador = obter_jogador_atual(e);
     e = inicializar_estado();
-    for ( i = 0; i < dado; i++) {
-        e->jogadas[i] = tmp[i];
+    faz_primeira_jogada(e);
+    for (int i = 1;i <= dado; i++){
+        COORDENADA c1 = t[i].jogador1;
+        altera_estado(e,c1);
+        COORDENADA c2 = t[i].jogador2;
+        altera_estado(e,c2);
     }
-    e->num_jogadas = dado;
-    e->num_movimentos = 2*dado;
-    e->jogador_atual = jogador;
-    e->ultima_jogada = tmp[i].jogador2;
     mostrar_tabuleiro(e);
-    printf("FEITO");
-    return 0;
+    movs(e);
+    e -> num_movimentos = m;
+    return e;
 }
+
