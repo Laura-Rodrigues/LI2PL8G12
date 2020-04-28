e2314b9cccee08bb8eab26550dc513cef90607a4
MarianaFilipa <62109899+MarianaFilipa@users.noreply.github.com>
2020-04-28 21:17:37 +0100

    Alteração das funções

--- Camada_dados.c
+++ Camada_dados.c
@@ -2,12 +2,8 @@
 // Created by laura on 12/03/20.
 //
 
-
 #include "Camada_dados.h"
 #include <stdlib.h>
-
-// AS ÚNICAS FUNÇÕES QUE ALTERAM O ESTADO SÃO AQUI;
-
 
 //Esta função deverá criar um estado vazio (com o tabuleiro inicializado)
 ESTADO *inicializar_estado(){
@@ -15,124 +11,120 @@ ESTADO *inicializar_estado(){
     int j, i;
     for ( i = 0; i < 8; i++){
         for ( j = 0; j < 8; j++){
-            if ( i == 3 && j == 4) e->tab[i][j] =  BRANCA;
-            else if ( i == 7 && j == 0) e->tab[i][j] = UM;
-            else if (i == 0 && j == 7) e->tab[i][j] = DOIS;
-            else e->tab[i][j] = VAZIO;
+            if      ( i == 3 && j == 4 ) e -> tab[i][j] =  BRANCA;
+            else if ( i == 7 && j == 0 ) e -> tab[i][j] = UM;
+            else if ( i == 0 && j == 7 ) e -> tab[i][j] = DOIS;
+            else e -> tab[i][j] = VAZIO;
         }
     }
-    COORDENADA coordenada_inicial = {4,4};
-    e->ultima_jogada = coordenada_inicial;
+    COORDENADA coord_inicial = {4,4};
+    e -> ultima_jogada = coord_inicial;
     COORDENADA jog1 = {4,4};
-    e->jogadas[0].jogador2 = jog1;
-    e->num_jogadas = 0;
-    e->jogador_atual = 2;
-    e->num_movimentos = 0;
-    e->alt_pos = 0;
+    e -> jogadas[0].jogador2 = jog1;
+    e -> num_jogadas = 0;
+    e -> jogador_atual = 2;
+    e -> num_movimentos = 0;
+    e -> alt_pos = 0;
     return e;
 }
 
 //Esta função permite obter o número do jogador atual
-int obter_jogador_atual(ESTADO *estado){
-    return (estado->jogador_atual);
+int obter_jogador_atual( ESTADO *estado ){
+    return ( estado -> jogador_atual );
 }
 
-
 //Esta função permite obter quantas jogadas foram efetuadas (cada jogada tem o movimento de dois jogadores).
-int obter_numero_de_jogadas(ESTADO *estado){
-    return (estado->num_jogadas);
+int obter_numero_de_jogadas( ESTADO *estado ){
+    return ( estado -> num_jogadas );
 }
 
 //Função que troca a coluna pela linha e vice-versa
-COORDENADA troca_ordem (COORDENADA c){
-    COORDENADA final = {7-c.linha, c.coluna};
+COORDENADA troca_ordem ( COORDENADA coord ){
+    COORDENADA final = {7-coord.linha, coord.coluna};
     return final;
 }
 
 //Esta função permite obter o estado atual da casa. Nota: esta função recebe as coordenadas no format "linha,coluna"
-CASA obter_estado_casa(ESTADO *e, COORDENADA c){
-    int i = c.coluna;
-    int j = c.linha;
-    CASA final = (e-> tab[i][j]);
+CASA obter_estado_casa( ESTADO *estado, COORDENADA coord ){
+    int col = coord.coluna, lin = coord.linha;
+    CASA final = ( estado -> tab[col][lin] );
     return final;
 }
 
 //Esta função deve aceder ao estado e returnar a ultima jogada;
-COORDENADA obter_ultima_jogada (ESTADO *estado){
+COORDENADA obter_ultima_jogada ( ESTADO *estado ){
     COORDENADA ultima;
-    ultima = estado->ultima_jogada;
+    ultima = estado -> ultima_jogada;
     return ultima;
 }
 
-int obter_num_mov (ESTADO *e){
-    return e->num_movimentos;
+int obter_num_mov ( ESTADO *estado ){
+    return estado -> num_movimentos;
 }
 
-COORDENADA obter_jogada (ESTADO *e, int njogada, int jogador){
-    COORDENADA r;
-    JOGADA jogada = e-> jogadas [njogada];
-    if ( jogador == 1) r = jogada.jogador1;
-    else r = jogada.jogador2;
-    return r;
+COORDENADA obter_jogada ( ESTADO *estado, int njogada, int jogador ){
+    COORDENADA result;
+    JOGADA jogada = estado -> jogadas [njogada];
+    if ( jogador == 1 ) result = jogada.jogador1;
+    else result = jogada.jogador2;
+    return result;
 }
 
-int obter_pos (ESTADO *e){
-    return e->alt_pos;
+int obter_pos ( ESTADO *estado ){
+    return estado -> alt_pos;
 }
 
-void aumenta_pos (ESTADO* e, int i){
-    e->alt_pos = i + 1;
+void aumenta_pos ( ESTADO *estado, int valor ){
+    estado -> alt_pos = valor + 1;
 }
 
-void faz_primeira_jogada (ESTADO *e){
-    altera_numjogadas(e);
-    altera_jogadoratual(e);
-    altera_num_mov(e);
+void altera_pos ( ESTADO *estado ){
+    estado -> alt_pos = 0;
 }
 
-void altera_pos (ESTADO* e){
-    e->alt_pos = 0;
+void altera_tabuleiro ( ESTADO *estado, COORDENADA coord ){
+    estado -> tab[ 7 - (coord.linha) ][ coord.coluna ] = BRANCA;
+    estado -> tab[ 7 - (estado->ultima_jogada).linha ][ (estado->ultima_jogada).coluna ] = PRETA;
 }
 
-void altera_tabuleiro (ESTADO *estado, COORDENADA c){
-    estado->tab[7-(c.linha)][c.coluna] = BRANCA;
-    estado->tab[7-(estado->ultima_jogada).linha][(estado->ultima_jogada).coluna]=PRETA;
+void altera_ultimajogada ( ESTADO *estado, COORDENADA coord ){
+    estado -> ultima_jogada = coord;
 }
 
-void altera_ultimajogada (ESTADO *e, COORDENADA c){
-    e->ultima_jogada = c;
+void altera_jogadas ( ESTADO *estado, COORDENADA coord ){
+    int ind = ( estado -> num_jogadas);
+    if ( estado -> jogador_atual == 1 )
+        estado -> jogadas[ind].jogador1 = coord;
+    else
+        estado -> jogadas[ind].jogador2 = coord;
 }
 
-void altera_jogadas (ESTADO *e, COORDENADA c){
-    int i = (e->num_jogadas);
-    if (e->jogador_atual == 1){
-        e->jogadas[i].jogador1 = c;
-    }
-    else {
-        e->jogadas[i].jogador2 = c;
-    }
+void altera_numjogadas ( ESTADO *estado ){
+    if ( estado -> jogador_atual == 2 ) estado -> num_jogadas ++;
 }
 
-
-void altera_numjogadas (ESTADO *e){
-    if ( e -> jogador_atual == 2) e->num_jogadas += 1;
+void altera_jogadoratual( ESTADO *estado ){
+    if ( estado -> jogador_atual == 1 ) estado -> jogador_atual = 2;
+    else estado -> jogador_atual = 1;
 }
 
-void altera_jogadoratual(ESTADO *e){
-    if (e->jogador_atual == 1) e->jogador_atual = 2;
-    else e->jogador_atual = 1;
+void altera_num_mov ( ESTADO *estado ){
+    estado -> num_movimentos++;
 }
 
-void altera_num_mov (ESTADO *e){
-    e->num_movimentos++;
+void faz_primeira_jogada ( ESTADO *estado ){
+    altera_numjogadas( estado );
+    altera_jogadoratual( estado );
+    altera_num_mov( estado );
 }
 
-void altera_estado (ESTADO *e, COORDENADA c){
-    altera_pos(e);
-    altera_tabuleiro(e, c);
-    altera_ultimajogada(e,c);
-    altera_jogadas(e, c);
-    altera_numjogadas(e);
-    altera_jogadoratual(e);
-    altera_num_mov(e);
+void altera_estado (ESTADO *estado, COORDENADA coord){
+    altera_pos( estado );
+    altera_tabuleiro( estado, coord );
+    altera_ultimajogada( estado, coord );
+    altera_jogadas( estado, coord );
+    altera_numjogadas( estado );
+    altera_jogadoratual( estado );
+    altera_num_mov( estado );
 }
+
