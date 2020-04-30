4ab0763ac995563b8f7aa1854f38b6b91c7dab4a
MarianaFilipa <62109899+MarianaFilipa@users.noreply.github.com>
2020-04-30 10:25:59 +0100

    Alteração das movs

--- Funcoes_Ficheiro.c
+++ Funcoes_Ficheiro.c
@@ -92,30 +92,6 @@ ESTADO *ler ( char nomeficheiro[], ESTADO *estado ){
     return estado;
 }
 
-int movs ( ESTADO *estado ) {
-    char col1, col2;
-    int ind, lin1, lin2, jogada = obter_numero_de_jogadas( estado ), jogador = obter_jogador_atual( estado );
-    COORDENADA jog1, jog2;
-    if ( jogador == 1 ) jogada--;
-    for ( ind = 1; ind <= jogada ; ind++ ) {
-        jog1 = obter_jogada( estado, ind, 1 );
-        jog2 = obter_jogada( estado, ind, 2 );
-        col1 = jog1.coluna + 'a';
-        lin1 = jog1.linha + 1;
-        col2 = jog2.coluna + 'a';
-        lin2 = jog2.linha + 1;
-        if ( ind < jogada )
-            printf( "%02d: %c%d %c%d \n", ind, col1, lin1, col2, lin2 );
-        else {
-            if ( jogador == 2)
-                printf( "%02d: %c%d \n", ind , col1, lin1 );
-            else
-                printf( "%02d: %c%d %c%d \n", ind, col1, lin1, col2, lin2 );
-        }
-    }
-    return 0;
-}
-
 ESTADO* pos (ESTADO *e, int dado){
     int m = obter_num_mov(e), i = obter_pos(e);
     JOGADAS t;
