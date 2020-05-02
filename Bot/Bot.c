243a0e5c666212a82a593e6891b26a98fc017570
MarianaFilipa <62109899+MarianaFilipa@users.noreply.github.com>
2020-05-03 00:38:54 +0100

    Update Bot.c

--- Bot/Bot.c
+++ Bot/Bot.c
@@ -7,29 +7,6 @@
 #include "Logica_Programa.h"
 #include "Funcoes_Ficheiro.h"
 
-/*
-COORDENADA jog ( ESTADO *e ){
-    COORDENADA ls[8], *c, t;
-    coordvizinho(e, ls);
-    LISTA L = criar_lista();
-    int i, j = obter_jogador_atual(e), max = 32, dist;
-    for ( i = 0; i < 8 ; i++) {
-        if (jogada_valida(e, ls[i]) && !(cond_canto(ls[i]))) {
-            dist = det_dist(ls[i], j);
-            if (dist < max) {
-                max = dist;
-                t = ls[i];
-            } else
-                L = insere_cabeca(L, ls + i);
-        }
-    }
-    L = insere_cabeca(L, &t);
-    c = (COORDENADA *) devolve_cabeca(L);
-    printf("A jogada recomendada é: %c%d. \n", c->coluna+'a', c->linha+1);
-    return *c;
-}
-*/
-
 //receber o estado do tabuleiro, analisar as jogadas possíveis,
 // aplicar uma heurística para escolher a melhor jogada e
 // jogar nessa jogada, devolvendo o tabuleiro atualizado.
