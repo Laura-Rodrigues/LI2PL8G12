a2e9cda56ae54fc6f57d7add9b1faa7a9059c1b5
MarianaFilipa <62109899+MarianaFilipa@users.noreply.github.com>
2020-05-03 19:15:18 +0100

    Update Logica_Programa.c

--- projeto/Logica_Programa.c
+++ projeto/Logica_Programa.c
@@ -6,10 +6,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Esta função deverá receber o estado atual e uma coordenada e modificar o estado ao jogar na casa correta se a jogada for válida.
- * A função devolve verdadeiro (valor diferente de zero) se for possível jogar e falso (zero) caso não seja possível.*/
-
-
 // Testa se a Coordenada é vizinha da Coordenada anterior e se encontra-se dentro do tabuleiro
 int e_vizinha( ESTADO *estado, COORDENADA coord){
     COORDENADA ultjogada = obter_ultima_jogada(estado);
